fd5be310777e7ac791969c8d357d89d69c416609
Sarthak Rastogi <saarthakrastogi8@gmail.com>
2026-09-08 23:40:01 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.java
@@ -0,0 +1,53 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
+
+        // Frequency of characters in s1
+        for (char c : s1.toCharArray()) {
+            count1[c - 'a']++;
+        }
+
+        int windowSize = s1.length();
+
+        // First window
+        for (int i = 0; i < windowSize; i++) {
+            count2[s2.charAt(i) - 'a']++;
+        }
+
+        if (matches(count1, count2)) {
+            return true;
+        }
+
+        // Slide the window
+        for (int right = windowSize; right < s2.length(); right++) {
+
+            // Add new character
+            count2[s2.charAt(right) - 'a']++;
+
+            // Remove old character
+            int left = right - windowSize;
+            count2[s2.charAt(left) - 'a']--;
+
+            if (matches(count1, count2)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean matches(int[] a, int[] b) {
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
